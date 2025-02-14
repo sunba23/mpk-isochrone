25b21aa4108de5cee29fc762d7f66852b1c59481
Franek Suszko <franeksu@gmail.com>
2025-02-14 21:42:27 +0100

    fix: allow for docker secrets in psql api connection

--- api/dao/config.go
+++ api/dao/config.go
@@ -1,6 +1,10 @@
 package dao
 
-import "os"
+import (
+	"os"
+	"path/filepath"
+	"strings"
+)
 
 type Config struct {
 	Host     string
@@ -10,12 +14,22 @@ type Config struct {
 	DBName   string
 }
 
+func getSecretOrEnv(secretName, envVar string) string {
+	secretPath := filepath.Join("/run/secrets", secretName)
+	data, err := os.ReadFile(secretPath)
+	if err == nil {
+		return strings.TrimSpace(string(data))
+	}
+
+	return os.Getenv(envVar)
+}
+
 func GetConfig() *Config {
 	return &Config{
-		Host:     os.Getenv("PG_HOST"),
+		Host:     getSecretOrEnv("PG_HOST", "PG_HOST"),
 		Port:     5432,
-		User:     os.Getenv("PG_USER"),
-		Password: os.Getenv("PG_PASSWORD"),
-		DBName:   os.Getenv("PG_DBNAME"),
+		User:     getSecretOrEnv("PG_USER", "PG_USER"),
+		Password: getSecretOrEnv("PG_PASSWORD", "PG_PASSWORD"),
+		DBName:   getSecretOrEnv("PG_DBNAME", "PG_DBNAME"),
 	}
 }
